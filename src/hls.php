18ce924a9dd5a2a1b782d8f66107acae95d28bc0
洪吕石 <hls@honglvshidemacbook-pro.local>
2017-10-28 18:16:31 +0800

    add jenkins test file

---
+++ src/hls.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hls
+ * Date: 2017/10/28
+ * Time: 下午6:05
+ */
+echo ' this is php file';
---
+++ test/hlsTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace test;
+use PHPUnit\Framework\TestCase;
+class hlsTest extends TestCase
+{
+    public function test1()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+    public function test2()
+    {
+       $this->assertEquals(2, 2);
+    }
+}
